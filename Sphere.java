66e076fb07ac29de785c0cd3ebfa6f08a00e98c4
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 23:44:03 +0530

    area of sphere

--- Sphere.java
+++ Sphere.java
@@ -2,5 +2,8 @@ class Sphere extends Shape  {
     public Sphere(double radius) {
         super(radius, 0, 0, 0,0);
     }
-
+    @Override
+    double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
 }
