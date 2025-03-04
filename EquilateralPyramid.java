c8afafbe1589caa92927a0a21cf2df9e0ad900d7
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-05 01:00:43 +0530

    area of pyramind

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -2,5 +2,9 @@ class EquilateralPyramid extends Shape implements Volume {
     public EquilateralPyramid(double base, double height) {
         super(base, height, 0,0, 4);
     }
-    
+    @Override
+    double calculateArea() {
+        return dim_one * dim_one;
+    }
+
 }
