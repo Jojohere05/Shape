f44e15622000c87ca708bd391093173925eec61a
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 23:31:09 +0530

    area circle

--- Circle.java
+++ Circle.java
@@ -2,5 +2,8 @@ class Circle extends Shape {
     public Circle(double radius){
         super(radius,0,0,0,0)
     }
-    
+    @Override
+    double calculatearea() {
+        return Math.PI * dim_one * dim_one;
+    }
 }
