1a4b3524728ffdd50ded9e2147ac78c297f93875
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 23:23:15 +0530

    zero constructor

--- Shape.java
+++ Shape.java
@@ -2,5 +2,9 @@ abstract class Shape {
     protected double dim_one, dim_two, dim_three, radius;
     int num_sides;
 
-   
+    public Shape() {
+        dim_one = dim_two = dim_three = radius = 0; // Default values
+        num_sides = 0;
+    }
+    
 }
