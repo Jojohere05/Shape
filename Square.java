b6a77d582725b081971f5006e004c578016642d7
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 23:38:59 +0530

    square.java area

--- Square.java
+++ Square.java
@@ -2,5 +2,8 @@ class Square extends Shape{
     public Square(double side) {
         super(side, side, 0,0, 4);
     }
-    
+    @Override
+    double calculateArea() {
+        return dim_one * dim_one;
+    }
 }
