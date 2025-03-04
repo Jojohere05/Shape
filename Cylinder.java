9c019995f72b751fe4bf60ff5e28cf447c9955dd
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-05 00:42:21 +0530

    perimeter and area of cylinder

--- Cylinder.java
+++ Cylinder.java
@@ -2,6 +2,14 @@ class Cylinder extends Shape implements Volume{
     public Cylinder(double radius, double height) {
         super(radius, height, 0, 0,0);
     }
+    @Override
+    double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_two);
+    }
 
+    @Override
+    double calculatePerimeter() {
+        return 0; // No perimeter for a cylinder
+    }
 
 }
